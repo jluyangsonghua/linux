8f366386442ea7072d5e25186191f829e891f222
jluyangsonghua <jluyangsonghua@163.com>
2022-02-23 10:30:46 +0800

    current的宏定义及原理

--- arch/arm64/include/asm/current.h
+++ arch/arm64/include/asm/current.h
@@ -12,11 +12,11 @@ struct task_struct;
  * We don't use read_sysreg() as we want the compiler to cache the value where
  * possible.
  */
-static __always_inline struct task_struct *get_current(void)
+static __always_inline struct task_struct *get_current(void)//current的定义
 {
 	unsigned long sp_el0;
 
-	asm ("mrs %0, sp_el0" : "=r" (sp_el0));
+	asm ("mrs %0, sp_el0" : "=r" (sp_el0));//是通过使用el0模式下的sp寄存器(因为此时是运行于内核模式，使用的栈寄存器是sp_el1)
 
 	return (struct task_struct *)sp_el0;
 }
