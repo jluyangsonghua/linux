34eac52bf60fee8da5a110d7a4a1188b2c2a6206
jluyangsonghua <jluyangsonghua@163.com>
2022-05-27 09:35:34 +0800

    readme: add readme

--- make.sh
+++
@@ -1,4 +0,0 @@
-!bin/sh
-gnu_dir=/home/ysh/code/toolchain/gcc-linaro-7.5.0-2019.12-rc1-x86_64_aarch64-linux-gnu/aarch64-linux-gnu
-CROSS_COMPILE_DIR=$gnu_dir/bin/aarch64-linux-gnu-
-make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE_DIR menuconfig
